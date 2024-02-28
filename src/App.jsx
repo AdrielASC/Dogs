917bf9747cfe6208e741bc8ae0b2aaefb44f77d8
AdrielASC <adriel.ascoelho@gmail.com>
2024-02-28 19:45:11 -0300

    Projeto Dogs Finalizado

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./User/User";
+import ProtectedRoute from "./Helper/ProtectedRoute";
+import Photo from "./Components/Photo/Photo";
+import UserProfile from "./User/UserProfile";
+import NotFound from "./Components/NotFound";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
